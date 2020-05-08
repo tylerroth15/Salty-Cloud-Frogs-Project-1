dd9e1b926866305d5bb399d8b330c7dab465822b
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-05-07 21:07:29 -0400

    Created the welcome function and the local storage for new and returning users

---
+++ userTracking.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    function welcome(){
+        
+        //add lines for hiding the game cards
+        $(".card").attr("style", "display:none")
+
+        //welcoming the user to the site.  If there's no value in local storage the user will be greeted as a stranger and asked to enter their username, which is then stored
+        var checkName = localStorage.getItem("name");
+        console.log(checkName);
+        if (checkName == null || checkName == "null"){
+            
+            $("#welcome").text("Hi, Stranger!");
+
+            $("#enterBtn").on("click", function(event) {
+                event.preventDefault();
+                console.log("clicked");
+                $("#newUserOnly").attr("style", "display:block")
+
+                var username = $("#get-username").val().trim();
+                localStorage.setItem("name", username);
+                $("#newUserOnly").attr("style", "display:none");
+                $("#welcome").text("Hello " + username + ", shall we play a game?");
+                $(".card").attr("style", "display:block")
+
+            });    
+        } else {
+            //Display "Hello " + username + " welcome back!"
+            $("#newUserOnly").attr("style", "display:none");
+            $("#welcome").text("Hello " + checkName + ", shall we play a game?");
+            $(".card").attr("style", "display:block");
+
+            //Not User?  Need a clickable link to clear Local Storage and re-run the welcome function.
+        } 
+    } 
+
+    welcome();
+
+});
