8c38266ec3ecc4607d1c11adab3ae08f9b781258
Tyler Roth <tylerroth1535@gmail.com>
2020-05-09 13:52:31 -0400

    updated the page css, made the card titles clickable, fixed the layout of the HTML and alligned the tic tac toe game.

--- ui.js
+++ ui.js
@@ -22,7 +22,7 @@ const render = function () {
 const renderTicTacToe = function () {
     for (let i = 0; i < 9; i++) {
         let newBtn = $("<button>");
-        let baseStlye = "width: 150px; height: 150px;";
+        let baseStlye = "width: 150px; height: 150px; font-size: 75px;";
         gameDiv
             .append(newBtn
                 .text("")
