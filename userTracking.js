4f310f9aa1c9b6f35ce60316020206f98144c85f
David Stewart <darkorin00@hotmail.com>
2020-05-09 10:48:34 -0400

    reorganization

--- userTracking.js
+++ userTracking.js
@@ -1,39 +1,33 @@
-$(document).ready(function() {
+const welcome = function () {
 
-    function welcome(){
-        
-        //add lines for hiding the game cards
-        $(".card").attr("style", "display:none")
+    //add lines for hiding the game cards
+    $(".card").attr("style", "display:none")
 
-        //welcoming the user to the site.  If there's no value in local storage the user will be greeted as a stranger and asked to enter their username, which is then stored
-        var checkName = localStorage.getItem("name");
-        console.log(checkName);
-        if (checkName == null || checkName == "null"){
-            
-            $("#welcome").text("Hi, Stranger!");
+    //welcoming the user to the site.  If there's no value in local storage the user will be greeted as a stranger and asked to enter their username, which is then stored
+    var checkName = localStorage.getItem("name");
+    console.log(checkName);
+    if (checkName == null || checkName == "null") {
 
-            $("#enterBtn").on("click", function(event) {
-                event.preventDefault();
-                console.log("clicked");
-                $("#newUserOnly").attr("style", "display:block")
+        $("#welcome").text("Hi, Stranger!");
 
-                var username = $("#get-username").val().trim();
-                localStorage.setItem("name", username);
-                $("#newUserOnly").attr("style", "display:none");
-                $("#welcome").text("Hello " + username + ", shall we play a game?");
-                $(".card").attr("style", "display:block")
+        $("#enterBtn").on("click", function (event) {
+            event.preventDefault();
+            console.log("clicked");
+            $("#newUserOnly").attr("style", "display:block")
 
-            });    
-        } else {
-            //Display "Hello " + username + " welcome back!"
+            var username = $("#get-username").val().trim();
+            localStorage.setItem("name", username);
             $("#newUserOnly").attr("style", "display:none");
-            $("#welcome").text("Hello " + checkName + ", shall we play a game?");
-            $(".card").attr("style", "display:block");
+            $("#welcome").text("Hello " + username + ", shall we play a game?");
+            $(".card").attr("style", "display:block")
 
-            //Not User?  Need a clickable link to clear Local Storage and re-run the welcome function.
-        } 
-    } 
+        });
+    } else {
+        //Display "Hello " + username + " welcome back!"
+        $("#newUserOnly").attr("style", "display:none");
+        $("#welcome").text("Hello " + checkName + ", shall we play a game?");
+        $(".card").attr("style", "display:block");
 
-    welcome();
-
-});
+        //Not User?  Need a clickable link to clear Local Storage and re-run the welcome function.
+    }
+}
